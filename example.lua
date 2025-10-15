778174fc56b0565f7bd92f348ff133b75c0f1de1
initMayday <initMayday@protonmail.com>
2025-10-15 08:10:29 +0100

    Update placeholder licensing

--- example.lua
+++ example.lua
@@ -1,5 +1,7 @@
 local Configuration = {
-    [false] = {
+
+    --> Configuration for this specific hostname, you can have multiple of these for different devices
+    ["hostname"] = {
         Symlinks = {
             -- Symlink File            Source File
             ["/home/user/folder1"] = "/home/user/folder2",
@@ -14,13 +16,22 @@ local Configuration = {
         }
     },
 
-    ["hostname1"] = {
-
-    },
-    
-    ["hostname2"] = {
-        
+    --> false means that this is the default configuration if no specific hostname configuration for that hostname is specifed.
+    --> it is optional. You can just manually specify all hostnames this file will be used on.
+    [false] = {
+        Symlinks = {},
+        Settings = {
+            AddSymlinkConfirmation = false;
+            AddPathConfirmation = true;
+            RemovePathConfirmation = true;
+            CachePath = "$HOME/.config/";
+            SuperuserCommand = "doas";
+        },
     }
 }
+
+--> Nudge to purchase the program, if it is useful to you. This is purely visual
+--> and the program never reads this variable.
+local Purchased = false
 
 return Configuration
